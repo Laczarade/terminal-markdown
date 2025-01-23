9e7f3271420ba67956837830151bccc14b73f23e
Laczarade <billjavandel@gmail.com>
2025-01-22 23:54:28 -0800

    Add bold-text recognition

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # terminal-markdown
 A Rust program to view formatted markdown in-terminal
+
+Currently, it can only recognize **bold text**, denoted by double asterisks.
--- tmd/src/main.rs
+++ tmd/src/main.rs
@@ -4,15 +4,40 @@ use std::fs::File;
 use std::io::{Write, BufReader, BufRead, Error};
 use std::path::Path;
 
+static HELPSCREEN: &str = "Usage: tmd filename";
+static BOLD: &str = "\x1b[1m";
+static ITALIC: &str = "\x1b[3m";
+static NORMAL: &str = "\x1b[0m";
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
+    if args.len() == 1 {
+        println!("{}", HELPSCREEN);
+        return Ok(()); // This will return an error in later versions
+    }
     let filepath = &args[1];
 
     let input = File::open(filepath)?;
     let buffered = BufReader::new(input);
 
+    let mut bold = false;
+    let mut italic = false;
     for line in buffered.lines() {
-        println!("{}", line?);
+        let mut line = line.unwrap_or(String::from(""));
+        // This loop will contain the majority of the important section of our code.
+        // First, we'll render any Bold text
+        while line.contains("**") {
+            println!("Processing bold");
+            if bold {
+                line = line.replacen("**", NORMAL, 1);
+                bold = false;
+            } else {
+                line = line.replacen("**", BOLD, 1);
+                bold = true;
+            }
+        }
+
+        println!("{}", line);
     }
 
     Ok(())
